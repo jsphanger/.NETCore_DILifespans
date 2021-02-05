08919d2a0d03e150565b1e4125707b55eac3896d
Joseph Anger <jsphanger@yahoo.com>
2021-02-05 09:45:37 -0600

    Initial Project Files
    - Added the initial project files

---
+++ DILifeSpans.Core/Models/DIServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DILifeSpans.Core.Models
+{
+    public interface ITransientService
+    {
+        Guid GetID();
+    }
+
+    public interface IScopedService
+    {
+        Guid GetID();
+    }
+
+    public interface ISingletonService
+    {
+        Guid GetID();
+    }
+
+    public class DIService : ITransientService, IScopedService, ISingletonService
+    {
+        public Guid ID { get; set; }
+        public DIService()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        public Guid GetID()
+        {
+            return ID;
+        }
+    }
+}
---
+++ DILifeSpans/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DILifeSpans.MVC.ViewModels;
+using DILifeSpans.Core.Models;
+
+namespace DILifeSpans.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransientService transitentOne, transientTwo;
+        private readonly IScopedService scopedOne, scopedTwo;
+        private readonly ISingletonService singletonOne, singletonTwo;
+        
+        public HomeController(ITransientService transientServiceOne, ITransientService transientServiceTwo, 
+                              IScopedService scopedServiceOne, IScopedService scopedServiceTwo, 
+                              ISingletonService singletonServiceOne, ISingletonService singletonServiceTwo)
+        {
+            transitentOne = transientServiceOne;
+            transientTwo = transientServiceTwo;
+
+            scopedOne = scopedServiceOne;
+            scopedTwo = scopedServiceTwo;
+
+            singletonOne = singletonServiceOne;
+            singletonTwo = singletonServiceTwo;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new HomeViewModel() { TransientOne = transitentOne.GetID(), TransientTwo = transientTwo.GetID(),
+                                              ScopedOne = scopedOne.GetID(), ScopedTwo = scopedTwo.GetID(),
+                                              SingletonOne = singletonOne.GetID(), SingletonTwo = singletonTwo.GetID() };
+
+            return View(model);
+        }
+    }
+}
